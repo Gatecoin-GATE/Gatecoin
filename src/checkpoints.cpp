939b6d4e35386e80f56e3e40ac3fd91c4a7472d7
Lenin <67344990+aSOVIET@users.noreply.github.com>
2020-08-17 04:41:22 -0300

    Update checkpoints.cpp

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -40,10 +40,10 @@ namespace Checkpoints
 		;
     static const CCheckpointData data = {
         &mapCheckpoints,
-        1596911972, // * UNIX timestamp of last checkpoint block
-        0,     // * total number of transactions between genesis and last checkpoint
+        1597649955, // * UNIX timestamp of last checkpoint block
+        //0,     // * total number of transactions between genesis and last checkpoint
                     //   (the tx=... number in the SetBestChain debug.log lines)
-        1.0     // * estimated number of transactions per day after checkpoint
+        150     // * estimated number of transactions per day after checkpoint
     };
 
     static MapCheckpoints mapCheckpointsTestnet = 
